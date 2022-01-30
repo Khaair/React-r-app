b66ca91c85ae230b31161dd095954f5ad14996b6
Khaair <khair.cse.iu@gmail.com>
2022-01-30 18:21:53 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import HelloUseState from "./Components/HelloUseState";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Profile from "./Components/Profile";
+
+
+
  function App() {
   return (
     <div className="App">
-      <HelloUseState></HelloUseState>
+     <nav className="navv">
+       <ul>
+         <li><Link to="/" className="ll">Home</Link></li>
+         <li><Link to="/contact" className="ll">Contact</Link></li>
+         <li><Link to="/About" className="ll">About</Link></li>
+         <li><Link to="/Profile" className="ll">Profile</Link></li>
+       </ul>
+     </nav>
+
+     <Routes>
+    <Route path="/" element= {<Home/>}/>
+    <Route path="/Contact" element= {<Contact/>}/>
+    <Route path="/About" element= {<About/>}/>
+    <Route path="/Profile" element= {<Profile/>}/>
+
+     </Routes>
 
     </div>
   );
